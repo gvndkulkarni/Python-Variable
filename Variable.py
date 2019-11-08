dcde68d72dc32f1d49ab811b6fb21359b2ba5b0e
Govind <gvndkulkarni5@gmail.com>
2019-11-08 16:53:22 +0530

    Variable in python

---
+++ Variable.py
@@ -0,0 +1,15 @@
+# Declare a variable and initialize it
+a = 100 
+print (a)
+
+# re-declaring the variable works
+a = 200
+print (a)
+
+# re-declaring the variable works
+a = 10000
+print(a)
+
+# re-declaring the variable works
+a = 'Govind'
+print (a)
